ca5b25749ca1c4fbb2e73c149f5bd1975c0c15a6
denglian <781307@nd.com>
2025-02-25 18:52:24 +0800

    chore: 支持动态构建pages.json

---
+++ src/pages.ts
@@ -0,0 +1,17 @@
+export const pagesJson = {
+  easycom: {
+    autoscan: true,
+    custom: {
+      '^uni-(.*)': '@dcloudio/uni-ui/lib/uni-$1/uni-$1.vue',
+    },
+  },
+  pages: [
+    // 将会自动塞入
+  ],
+  globalStyle: {
+    navigationBarTextStyle: 'black',
+    navigationBarTitleText: 'uni-app',
+    navigationBarBackgroundColor: '#F8F8F8',
+    backgroundColor: '#F8F8F8',
+  },
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
+import { exec } from 'node:child_process'
+import path from 'node:path'
 import uni from '@dcloudio/vite-plugin-uni'
+
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
@@ -10,5 +13,14 @@ export default defineConfig({
   },
   plugins: [
     uni(),
+    {
+      name: 'run-script-on-change',
+      handleHotUpdate({ file }) {
+        if (file === path.resolve(__dirname, 'src/pages.ts')) {
+          console.log('pages.ts changed')
+          exec('pnpm run build:pages')
+        }
+      },
+    },
   ],
 })
